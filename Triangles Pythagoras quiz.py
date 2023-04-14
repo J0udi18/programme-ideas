74349e13551d92a1b6cf185cf541a21a2bf67c8a
J0udi18 <101606075+J0udi18@users.noreply.github.com>
2023-04-14 19:40:10 +1200

    Add files via upload

---
+++ Triangles Pythagoras quiz.py
@@ -0,0 +1,36 @@
+import random
+
+score = 0
+
+print("Welcome to the Pythagoras Quiz!")
+print("You will be given a right triangle and you need to find the length of the missing side.")
+
+while True:
+    a = random.randint(1, 10)
+    b = random.randint(1, 10)
+    c = round((a ** 2 + b ** 2) ** 0.5, 2)
+    
+    sides = [a, b, c]
+    random.shuffle(sides)
+    
+    print(f" {a} cm {b} cm ?")
+    
+    answer = input("What is the length of the missing side? (Enter 'q' to quit) ")
+    
+    if answer.lower() == "q":
+        print(f"Your final score is {score}")
+        break
+    
+    try:
+        answer = float(answer)
+        if answer == c:
+            print("Correct!")
+            score += 1
+        else:
+            print(f"Incorrect. The correct answer is {c} cm.")
+    except ValueError:
+        print("Invalid input. Please enter a number or 'q' to quit.")
+
+# This code generates random right triangles and prompts the user to find 
+# the length of the missing side. It also keeps track of the score and allows
+# the user to quit at any time by entering 'q
