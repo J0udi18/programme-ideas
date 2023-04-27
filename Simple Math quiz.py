f222c12ba1c96e4b8c5b956991c3802fe5b407da
J0udi18 <101606075+J0udi18@users.noreply.github.com>
2023-04-28 00:02:06 +1200

    Add files via upload

--- Simple Math quiz.py
+++ Simple Math quiz.py
@@ -1,6 +1,5 @@
+from itertools import count
 import random
-
-
 def display_tittle():
     tittle = "Simple Math Quiz"
     print("*" * len(tittle))
@@ -35,17 +34,40 @@ def get_porblem(operation):
     num1 = random.randint(1, 10)
     num2 = random.randint(1, 10)
     if operation==1:
-        print("nu")
+        problem=f"{num1}+{num2}"
+        solution=num1+num2
+    elif operation==2:
+        problem=f"{num1}-{num2}"
+        solution=num1-num2
+    elif operation==3:
+        problem=f"{num1}*{num2}"
+        solution=num1*num2
+    elif operation==1:
+        problem=f"{num1}/{num2}"
+        solution=num1/num2
+    return problem,solution
 
 
 def main():
     display_tittle()
     display_options()
     option = get_userinput()
-    print(option)
-    prob = "2+6"
-    user_answer = get_userinput(prob)
-    print("user answer")
-
+    count=0
+    total=0
+    while option !=5:
+        prob, solution=get_porblem(option)
+        user_answer = get_userinput(prob)
+        if user_answer==solution:
+            print("correct")
+            count+=1
+            total+=1
+        else:
+            print("incorrect")
+            total+=1
+            option = get_userinput()
+            print("-"*30)
+    print("total questions asked", total)
+    print("corectly answered:", count)
+    print("percentage correctly answered", round(count*100/total,1))
 
 main()
