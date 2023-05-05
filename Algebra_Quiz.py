61329962eeaa6f5f3d7b74070996e90724bfbff1
J0udi18 <101606075+J0udi18@users.noreply.github.com>
2023-05-05 13:28:17 +1200

    Add files via upload

---
+++ Algebra_Quiz.py
@@ -0,0 +1,25 @@
+import random
+import math
+
+
+# checks if user knows algebra or
+# have an idea what is the quiz is going to be about
+def yes_no(question):
+    while True:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+
+        elif response == "no" or response == "n":
+            return "no"
+
+        else:
+            print("please answer yes / no")
+
+
+# display instructions
+def instructions():
+    print("Instructions")
+    print('''
+    ''')
